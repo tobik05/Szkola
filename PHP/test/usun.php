20c0ba51e4a5250ae2581bcff8ef39168c49fc8c
tobik05 <worldot132@gmail.com>
2024-03-06 09:32:35 +0100

    Update usun.php

--- PHP/test/usun.php
+++ PHP/test/usun.php
@@ -1,3 +1,13 @@
 <?php 
-    
+    $id = $_GET['id'];
+    $conn = mysqli_connect("localhost", "root", "", ) or die(mysqli_error($conn));
+    $db_select = mysqli_select_db($conn, "test") or die(mysqli_error($conn));
+    $sql = "ALTER TABLE mieszkancy DROP COLUMN WHERE {$id};";
+    $result = $conn->query($sql);
+    $row = mysqli_num_rows($result);
+    if ($result == true ) {
+        header();
+    } else{
+
+    }
 ?>
