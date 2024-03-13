a09cf8b7ebd6ad5d5efdaeff17a287b9e71582a8
tobik05 <worldot132@gmail.com>
2024-03-13 11:22:55 +0100

    fsdfsdf

--- Strony/formularze/script.js
+++ Strony/formularze/script.js
@@ -3,26 +3,30 @@ function zadanie1(){
     var tekst = DANE.tekst.value;
     var czcionka = DANE.czcionka.value;
     var rozmiar = DANE.rozmiar.value;
+    var grubosc = DANE.grubosc.value;
+    var kolor=DANE.kolor.value;
+    var pochylona = document.getElementById("pochylona");
+    var przekreslona = document.getElementById("przekreslona");
+    var podkreslona = document.getElementById("podkreslona");
+    var pogrubiona = document.getElementById("pogrubiona");
+    pochylona.checked==false, przekreslona.checked==false, podkreslona.checked==false, pogrubiona.checked==false;
 
-    var dekoracja_czcionki = "";
-    var dekoracja = document.forms["DANE"]["dekoracja"];
-    for (var i=0; i<dekoracja.length; i++) {
-        if (dekoracja[i].checked) {
-            dekoracja_czcionki[i] = dekoracja[i].value+" ";
-        }
-    }
-    console.log(dekoracja_czcionki);
-    kolor=DANE.kolor.value;
     wynik.innerHTML=tekst;
     document.getElementById("wynik").style.fontFamily=czcionka;
     document.getElementById("wynik").style.fontSize=rozmiar;
-    if(dekoracja_czcionki[0]!=""){
+    document.getElementById("wynik").style.border=grubosc+" solid";
+    if(pochylona.checked==true){
         document.getElementById("wynik").style.fontStyle="oblique";
-    } else if(dekoracja_czcionki[1]!=""){
+    } 
+    if(przekreslona.checked==true){
         document.getElementById("wynik").style.textDecoration="line-through";
-    } else if(dekoracja_czcionki[2]!=""){
+    } 
+    if(podkreslona.checked==true){
         document.getElementById("wynik").style.textDecoration="underline";
-    } else if(dekoracja_czcionki[3]!=""){
+    } 
+    if(pogrubiona.checked==true){
         document.getElementById("wynik").style.fontWeight="600";
-    }
+    } else document.getElementById("wynik").style.fontWeight="400";
+
+    document.getElementById("wynik").style.borderColor=kolor;
 }
