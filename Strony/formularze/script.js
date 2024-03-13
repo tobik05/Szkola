ab55080e936866314f9e8af65c7f7ab247d4cbb0
tobik05 <61117674+tobik05@users.noreply.github.com>
2024-03-13 14:14:44 +0100

    dasdasd

--- Strony/formularze/script.js
+++ Strony/formularze/script.js
@@ -3,8 +3,21 @@ function zadanie1(){
     var tekst = DANE.tekst.value;
     var czcionka = DANE.czcionka.value;
     var rozmiar = DANE.rozmiar.value;
-    var grubosc = DANE.grubosc.value;
-    var kolor=DANE.kolor.value;
+    var gruboscgora = DANE.gruboscgora.value;
+    var gruboscdol = DANE.gruboscdol.value;
+    var grubosclewo = DANE.grubosclewo.value;
+    var gruboscprawo = DANE.gruboscprawo.value;
+    var kolorgora=DANE.kolorgora.value;
+    var kolordol=DANE.kolordol.value;
+    var kolorlewo=DANE.kolorlewo.value;
+    var kolorprawo=DANE.kolorprawo.value;
+    var stylgora=DANE.stylgora.value;
+    var styldol=DANE.styldol.value;
+    var styllewo=DANE.styllewo.value;
+    var stylprawo=DANE.stylprawo.value;
+    var wypelnienie=DANE.wypelnienie.value;
+    var kolorczcionki=DANE.kolorczcionki.value;
+
     var pochylona = document.getElementById("pochylona");
     var przekreslona = document.getElementById("przekreslona");
     var podkreslona = document.getElementById("podkreslona");
@@ -14,7 +27,13 @@ function zadanie1(){
     wynik.innerHTML=tekst;
     document.getElementById("wynik").style.fontFamily=czcionka;
     document.getElementById("wynik").style.fontSize=rozmiar;
-    document.getElementById("wynik").style.border=grubosc+" solid";
+    document.getElementById("wynik").style.borderTop=gruboscgora+" "+stylgora+" "+kolorgora;
+    document.getElementById("wynik").style.borderBottom=gruboscdol+" "+styldol+" "+kolordol;
+    document.getElementById("wynik").style.borderLeft=grubosclewo+" "+styllewo+" "+kolorlewo;
+    document.getElementById("wynik").style.borderRight=gruboscprawo+" "+stylprawo+" "+kolorprawo;
+    document.getElementById("wynik").style.backgroundColor=wypelnienie;
+    document.getElementById("wynik").style.color=kolorczcionki;
+
     if(pochylona.checked==true){
         document.getElementById("wynik").style.fontStyle="oblique";
     } 
@@ -28,5 +47,4 @@ function zadanie1(){
         document.getElementById("wynik").style.fontWeight="600";
     } else document.getElementById("wynik").style.fontWeight="400";
 
-    document.getElementById("wynik").style.borderColor=kolor;
 }
