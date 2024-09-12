cf6cbb696bd7e27097af108cda0271c57d9aa885
tobik05 <61117674+tobik05@users.noreply.github.com>
2024-09-12 12:48:03 +0200

    dff

---
+++ kopia/index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Tobiasz, Bielawski, Strony, PHP">
+        <meta name="author" content="Tobiasz Bielawski">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/x-icon" href="logo.png">
+        <title>Tobiasz Bielawski</title>
+    </head>
+    <body>
+        <main>
+        </main>
+    </body>
+</html>
