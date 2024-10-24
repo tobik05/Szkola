3d0c28d2b5c8077fdc10e26b6987a789b3db9bbd
tobik05 <61117674+tobik05@users.noreply.github.com>
2024-10-24 11:05:41 +0200

    dsd

---
+++ P_Roj/php_funkcje/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funkcje PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <form method="post">
+            <input type="text" name="imie" placeholder="Podaj swoje imię">
+            <input type="submit" name="strlen" value="Wykonaj">
+        </form>
+        <?php
+        if(isset($_POST['strlen'] )){
+            echo "<h1>".strlen($_POST['imie'])."</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="ciag" placeholder="Podaj ciąg znakow">
+            <input type="text" name="podciag_tekst" placeholder="Podaj podciąg znakow">
+            <input type="submit" name="podciag" value="Wykonaj">
+        </form>
+        <?php
+        if(isset($_POST['podciag'])){
+            if(strpos($_POST['ciag'],$_POST['podciag_tekst'])!== false){
+                echo "<h1>Podciąg ".$_POST['podciag_tekst']." został znaleziony</h1>";
+            }else{
+                echo "<h1>Podciąg ".$_POST['podciag_tekst']." nie został znaleziony</h1>";
+            }
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="ciag_fragment" placeholder="Podaj ciąg znaków">
+            <input type="number" name="pozycja_poczatkowa" placeholder="Pozycja początkowa">
+            <input type="number" name="dlugosc" placeholder="Długość fragmentu">
+            <input type="submit" name="fragment" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['fragment'])) {
+            echo "<h1>Fragment: " . substr($_POST['ciag_fragment'], ($_POST['pozycja_poczatkowa']-1), $_POST['dlugosc']) . "</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="text" placeholder="Podaj tekst">
+            <input type="text" name="stare" placeholder="Podaj stare slowo">
+            <input type="text" name="nowe" placeholder="Podaj nowe slowo">
+            <input type="submit" name="replace" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['replace'])) {
+            echo "<h1>" . str_replace($_POST['stare'], $_POST['nowe'], $_POST['text']) . "</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="text" placeholder="Podaj tekst">
+            <input type="text" name="text_usuniecie" placeholder="Podaj znaki, ktore maja byc skasowane">
+            <input type="submit" name="trim" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['trim'])) {
+            echo "<h1>Oczyszczony tekst: '" . trim($_POST['text'], $_POST['text_usuniecie']) . "'</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="text" placeholder="Podaj tekst">
+            <input type="submit" name="wielkosc" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['wielkosc'])) {
+            echo "<h1>Wielkie litery: " . strtoupper($_POST['text']) . "</h1>";
+            echo "<h1>Małe litery: " . strtolower($_POST['text']) . "</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="text" placeholder="Podaj tekst">
+            <input type="submit" name="ucwords" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['ucwords'])) {
+            echo "<h1>" . ucfirst($_POST['text']) . "</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="text" placeholder="Podaj zdanie">
+            <input type="submit" name="ucwords2" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['ucwords2'])) {
+            echo "<h1>" . ucwords($_POST['text']) . "</h1>";
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="text" placeholder="Podaj zdanie">
+            <input type="submit" name="explode" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['explode'])) {
+            $slowa = explode(" ", $_POST['text']);
+            foreach ($slowa as $slowo) {
+                echo "<h1>$slowo</h1>";
+            }
+        }
+        ?>
+        <form method="post">
+            <input type="text" name="implode_words" placeholder="Podaj słowa (oddzielone przecinkami)">
+            <input type="submit" name="implode" value="Wykonaj">
+        </form>
+        <?php
+        if (isset($_POST['implode'])) {
+            $wordsArray = explode(",", $_POST['implode_words']);
+            echo "<h1>" . implode(", ", $wordsArray) . "</h1>";
+        }
+        ?>
+    </main>
+</body>
+</html>
