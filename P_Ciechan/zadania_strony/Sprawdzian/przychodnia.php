b67223b01de0484994d199b1a22c344ae2c0b2dd
tobik05 <61117674+tobik05@users.noreply.github.com>
2024-10-25 10:45:51 +0200

    s

--- P_Ciechan/zadania_strony/Sprawdzian/przychodnia.php
+++ P_Ciechan/zadania_strony/Sprawdzian/przychodnia.php
@@ -8,30 +8,87 @@
 </head>
 <body>
     <header>
-        
+        <h1>PRZYCHODNIA LEKARSKA</h1>
     </header>
     <section class="left_side">
-        <h1>PACJENCI:</h1>
+        <h2>PACJENCI:</h2>
+        <section class="pacjenci">
         <?php
-
+        $con=mysqli_connect("localhost","root","","przychodnia");
+        $zapytanie=mysqli_query($con,"SELECT Imie, Nazwisko, Pesel, Data_ur FROM pacjent");
+        echo "<table><tr><th>Imię</th><th>Nazwisko</th><th>PESEL</th><th>Data urodzenia</th></tr>";
+        while($row=mysqli_fetch_array($zapytanie)){
+            echo "<tr><td>".$row['Imie']."</td><td>".$row['Nazwisko']."</td><td>".$row['Pesel']."</td><td>".$row['Data_ur']."</td></tr>";
+        }
+        echo "</table>";
+        mysqli_close($con);
         ?>
+        </section>
     </section>
     <section class="center_side">
+        <h2>DODAJ PACJENTA DO BAZY:</h2>
         <form method="post">
-            Imię <input type="text" name="imie" required minlength="1">
-            Nazwisko <input type="text" name="nazwisko" required minlength="1">
-            PESEL <input type="number" name="pesel" required minlength="11" maxlength="11">
-            Data urodzenia <input type="number" name="data_urodzenia" required minlength="6" maxlength="6">
+            <p>Imię <input type="text" name="imie" required minlength="1"></p>
+            <p>Nazwisko <input type="text" name="nazwisko" required minlength="1"></p>
+            <p>PESEL <input type="number" name="pesel" required minlength="11"></p>
+            <p>Data urodzenia <input type="text" name="data_urodzenia" required minlength="6"></p>
+            <input type="submit" name="dodaj_do_bazy" value="DODAJ DO BAZY">
         </form>
+        <?php 
+        if(isset($_POST['dodaj_do_bazy'])){
+            $imie=$_POST['imie'];
+            $nazwisko=$_POST['nazwisko'];
+            $pesel=$_POST['pesel'];
+            $data_urodzenia=$_POST['data_urodzenia'];
+            $con=mysqli_connect("localhost","root","","przychodnia");
+            $zapytanie=mysqli_query($con,"INSERT INTO pacjent (Imie, Nazwisko, Pesel, Data_ur) VALUES('$imie', '$nazwisko', '$pesel', '$data_urodzenia')");    
+            mysqli_close($con);
+        }
+        ?>
     </section>
     <section class="right_side">
-        
+    <h2>SZUAKJ LEKARZA:</h2>
+        <form method="post">
+            <p><input type="text" required name="specjalnosc"></p>
+            <input type="submit" name="pokaz_specjalnosc" value="POKAŻ">
+        </form>
+        <?php
+        if(isset($_POST['pokaz_specjalnosc'])){
+            $specjalnosc=$_POST['specjalnosc'];
+            $con=mysqli_connect("localhost","root","","przychodnia");
+            $zapytanie=mysqli_query($con, "select imie, nazwisko from lekarz where specjalnosc='$specjalnosc'");
+            echo"<ul>";
+            while($row=mysqli_fetch_array($zapytanie)){
+                echo"<li>".$row['imie']." ".$row['nazwisko']."</li>";
+            }
+            echo "</ul>";
+            mysqli_close($con);
+        }
+        ?>
     </section>
     <section class="left_side_bottom">
-
+        <p>AKTUALNA LICZBA PACJENTÓW W BAZIE:</p>
+        <?php 
+        $con=mysqli_connect("localhost","root","","przychodnia");
+        $zapytanie=mysqli_query($con,"select count(*) as ile from pacjent");  
+        $row=mysqli_fetch_array($zapytanie);
+        echo $row['ile'];
+        mysqli_close($con);
+        
+        ?>
     </section>
     <section class="right_side_bottom">
-
+        <p>LISTA LEKARZY ORAZ LICZBA WIZYT KAŻDEGO Z NICH:</p>
+        <section class="wizyty">
+        <?php
+        $con=mysqli_connect("localhost","root","","przychodnia");
+        $zapytanie=mysqli_query($con, "SELECT Imie, Nazwisko, COUNT(wizyta.id_lekarza) as ile FROM lekarz INNER JOIN wizyta on wizyta.id_lekarza=lekarz.id_lekarza GROUP by lekarz.id_lekarza;");
+        while($row=mysqli_fetch_array($zapytanie)){
+            echo"<p>".$row['Imie']." ".$row['Nazwisko']." ".$row['ile']."</p>";
+        }
+        mysqli_close($con);
+        ?>
+        </section>
     </section>
     <footer>
         <h3>Imię nazwisko: Tobiasz Bielawski</h3>
