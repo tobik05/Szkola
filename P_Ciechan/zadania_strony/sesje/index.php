1f0132695d6f3bd4c74038f89de4ada003878c59
tobik05 <61117674+tobik05@users.noreply.github.com>
2024-11-08 11:20:42 +0100

    dsdsd

--- P_Ciechan/zadania_strony/sesje/index.php
+++ P_Ciechan/zadania_strony/sesje/index.php
@@ -28,15 +28,22 @@
     }
     $_SESSION['odwiedziny']++;
     echo "<p>Jesteś tutaj poraz: ".$_SESSION['odwiedziny'].".</p>";
+    if(!isset($_SESSION['zalogowany'])){
+    echo" <a href='login.php' class='button'>Zaloguj sie</a> <a href='rejestracja.php' class='button'>Rejestracja</a>";
+    }
+    echo "<a ";
+    if(isset($_SESSION['typ'])){
+        if($_SESSION['typ']=="admin"){
+            echo "class='button' href='strona_admin.php'>Strona admina - masz dostęp";
+        }
+    }else echo "class='button block-button' disabled>Strona admina -  nie masz dostępu";
+    echo "</a><a>";
     ?>
-    
 
-    <a href="login.php" class="button">Zaloguj sie</a>
-    <a href="rejestracja.php" class="button">Rejestracja</a>
-    <a href="strona.php" 
-        <?php if(isset($_SESSION['zalogowany'])){
-            echo "class='button'>Strona zablokowana - masz dostęp";
-        } else echo "class='button'>Strona zablokowana -  nie masz dostępu";
+        <?php 
+        if(isset($_SESSION['zalogowany'])){
+            echo "class='button' href='strona.php'>Strona zablokowana - masz dostęp";
+        }else echo "class='button block-button' disabled>Strona zablokowana -  nie masz dostępu";
         ?>
     </a>
     <a href="wyloguj.php" class="button">Wyloguj sie</a>
