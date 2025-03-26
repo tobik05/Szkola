e452e6420917682a9fc7250935a94f77aeef4d04
tobik05 <61117674+tobik05@users.noreply.github.com>
2025-03-26 04:40:37 +0100

    Update index.php

--- Informatyka/24.03/index.php
+++ Informatyka/24.03/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Symulacja Meczu Tenisa</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h2>Symulacja Meczu Tenisa</h2>
